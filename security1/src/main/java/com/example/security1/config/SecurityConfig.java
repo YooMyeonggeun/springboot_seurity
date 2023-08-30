ce68c6cafa054bfbaef9e1cfc41fa96bd9325380
UMK <haga5032@naver.com>
2023-08-30 20:46:20 +0900

    스프링시큐리티 3.0 권한 문제 해결

--- security1/src/main/java/com/example/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/example/security1/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성, preAuthorize 어노테이션 활성화
 public class SecurityConfig {
 
 
@@ -24,18 +25,9 @@ public class SecurityConfig {
     public SecurityFilterChain config(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().disable();
 
-
-//        http.authorizeHttpRequests(request -> request
-//                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-//                        .anyRequest("/user/**").authenticated()
-//                       .anyRequest().authenticated() // 어떠한 요청이라도 인증필요
-//        ).formLogin(login -> login.defaultSuccessUrl("", true).permitAll())
-//                .logout(Customizer.withDefaults());
-
-
-        http.authorizeHttpRequests().requestMatchers("/user/**").authenticated()
-                .requestMatchers("/manager/**").hasRole("admin or manager")
-                .requestMatchers("/admin/**").hasRole("admin")
+        http.authorizeHttpRequests().requestMatchers("/user/**").authenticated() // 인증만 되면 들어갈 수 있는 주소
+                .requestMatchers("/manager/**").hasAnyAuthority("manager", "admin")
+                .requestMatchers("/admin/**").hasAnyAuthority("admin")
                 .anyRequest().permitAll()
                 .and().formLogin().loginPage("/loginForm")
                 .loginProcessingUrl("/login") //login 주소가 호출이 되면 시큐리티가 낚아채서 대시 로그인을 진행해준다 controller에 /login을 만들지 않아도 됨
--- security1/src/main/java/com/example/security1/controller/IndexController.java
+++ security1/src/main/java/com/example/security1/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.example.security1.controller;
 import com.example.security1.entity.User;
 import com.example.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
-
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"", "/"})
-    @ResponseBody
     public String index() {
         // 머스테치 기본폴더 src/main/resources/
         // 뷰리졸버 설정 : templates(prefix), .mustache(suffix)
@@ -67,5 +66,18 @@ public class IndexController {
         return "redirect:/loginForm";
     }
 
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    @ResponseBody
+    public String info(){
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER')")
+    @GetMapping("/data")
+    @ResponseBody
+    public String data(){
+        return "데이터정보";
+    }
 
 }
