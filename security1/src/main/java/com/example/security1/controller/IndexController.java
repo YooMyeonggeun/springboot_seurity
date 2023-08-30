8144ef8e1588de872142874cbceec0058eec09dc
UMK <haga5032@naver.com>
2023-08-30 14:18:29 +0900

    스프링시큐리티 기본설정 완료

---
+++ security1/src/main/java/com/example/security1/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping({"", "/"})
+    public String index() {
+        // 머스테치 기본폴더 src/main/resources/
+        // 뷰리졸버 설정 : templates(prefix), .mustache(suffix)
+        // 생략가능 기본설정이기 때문
+        return "index"; // src/main/resources/templates/index.mustache
+    }
+}
